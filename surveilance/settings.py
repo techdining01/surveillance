05655376589e11fdc0bd965800e10cf528cf7c3f
techdining01 <amooade12@gmail.com>
2024-11-20 05:42:31 +0100

    contd

--- surveilance/settings.py
+++ surveilance/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'weather',
+    'traffic_jams_detours',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'surveillance/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
---
+++ traffic_jams_detours/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TrafficJamsDetoursConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'traffic_jams_detours'
